f92b3e77b12da5eac4965e1972f98dcf4a34849e
tal <talitamoss@proton.me>
2025-09-08 06:32:43 +0000

    Replace Mastodon branding with Kronk logo and resize

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,19 +1,11 @@
-import logo from '@/images/logo.svg';
-
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
-  </svg>
+  <img src='/logo.png' alt='Kronk' className='logo logo--wordmark' />
 );
 
 export const IconLogo: React.FC = () => (
-  <svg viewBox='0 0 79 79' className='logo logo--icon' role='img'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-icon' />
-  </svg>
+  <img src='/logo.png' alt='Kronk' className='logo logo--icon' />
 );
 
 export const SymbolLogo: React.FC = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src='/logo.png' alt='Kronk' className='logo logo--icon' />
 );
